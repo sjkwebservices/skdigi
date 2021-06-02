e81c6fd91198ac25eedc334645da59fc5d185336
stevok70 <sjk70@hotmail.com>
2021-06-02 23:30:20 +1000

    more fluent

--- sjkwebservices/sjkwebapp/src/index.tsx
+++ sjkwebservices/sjkwebapp/src/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { App } from './App';
+import { mergeStyles } from '@fluentui/react';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Inject some global styles
+mergeStyles({
+  ':global(body,html,#root)': {
+    margin: 0,
+    padding: 0,
+    height: '100vh',
+  },
+});
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
